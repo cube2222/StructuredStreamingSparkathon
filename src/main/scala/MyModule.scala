68946cccb35ed46ebdad90454d423c14d52f2962
Jacob Martin <jakub.wit.martin@gmail.com>
2016-11-10 19:55:51 +0100

    More descriptive batch.

--- src/main/scala/MyModule.scala
+++ src/main/scala/MyModule.scala
@@ -3,12 +3,15 @@
   */
 import java.sql.Timestamp
 
+import org.apache.log4j.{Level, LogManager}
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.execution.streaming.MemoryStream
 import org.apache.spark.sql.streaming.OutputMode
 
 object MyModule extends App {
+
+  LogManager.getLogger("org").setLevel(Level.OFF)
 
     val spark = SparkSession.builder().config("spark.sql.warehouse.dir", "file:///c:/tmp/spark-warehouse").master("local").getOrCreate()
     case class TimedEvent(id: Long, value: Int, timestamp: Timestamp)
@@ -28,11 +31,12 @@ object MyModule extends App {
       .outputMode(OutputMode.Complete)
       .start
 
+  (0 to 5).foreach { x =>
     intsInput.addData(
-      TimedEvent(123123, 5, new Timestamp(System.currentTimeMillis())),
-      TimedEvent(123123, 1, new Timestamp(System.currentTimeMillis() + 2000)),
-      TimedEvent(124412, 5, new Timestamp(System.currentTimeMillis() + 4000)),
-      TimedEvent(141, 3, new Timestamp(System.currentTimeMillis() + 20000)))
+      TimedEvent(123123, x, new Timestamp(System.currentTimeMillis())),
+      TimedEvent(1231234, x+1, new Timestamp(System.currentTimeMillis() + 2000)))
+    Thread.sleep(3000)
+  }
     query.awaitTermination()
 
 
